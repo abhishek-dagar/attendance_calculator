654f14a051d8cadd2ef954f05b64f69cfc62053e
abhishek-dagar <59116285+abhishek-dagar@users.noreply.github.com>
2020-02-26 21:09:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ from flask import Flask, render_template, request, session,redirect
 
 from Student import Student
 
-import os
-...
-port = int(os.environ.get('PORT', 5000))
 #########################################################
 class Scrape(object):
     def __init__(self):
@@ -127,7 +124,7 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
     '''while True:
         print('******************************')
         username = input('username: ')
